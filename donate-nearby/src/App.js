098fd3de00e2920280d7e5061fb0fddd14228c91
Remek-P <remek.piotrowski@wp.pl>
2022-11-29 18:14:10 +0100

    Initial routing Improved scss files structure

--- donate-nearby/src/App.js
+++ donate-nearby/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./scss/main.scss"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Layout } from "./components/Layout/Layout";
+import { Home } from "./pages/Home/Home";
+import { Form } from "./pages/Form/Form";
+import { Login } from "./pages/Login/Login";
+import { Logout } from "./pages/Logout/Logout";
+import { Register } from "./pages/Register/Register";
+import { MissingPage } from "./pages/MissingPage/MissingPage";
 
 function App() {
   return (
-    <div>test</div>
+    <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Layout />}>
+              <Route index element={<Home />}></Route>
+              <Route path={"form"} element={<Form />}></Route>
+              <Route path={"login"} element={<Login />}></Route>
+              <Route path={"logout"} element={<Logout />}></Route>
+              <Route path={"register"} element={<Register />}></Route>
+              <Route path={"*"} element={<MissingPage />}></Route>
+          </Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ donate-nearby/src/pages/MissingPage/MissingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function MissingPage() {
+    return(
+        <div>404</div>
+        )
+}
