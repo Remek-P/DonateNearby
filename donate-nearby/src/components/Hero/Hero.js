33c5c287e3e6755d04e8999dca232b43a36fd108
Remek-P <remek.piotrowski@wp.pl>
2022-12-03 17:00:00 +0100

    Hero Component - static Statistics component done - static

--- donate-nearby/src/components/Hero/Hero.js
+++ donate-nearby/src/components/Hero/Hero.js
@@ -4,19 +4,20 @@ import { Link } from "react-router-dom";
 export function Hero() {
     return (
         <section className="hero">
-            <h1 className="hero__header">
-                <div>Zacznij pomagać!</div>
-                <div>Oddaj niechciane rzeczy w zaufane ręce</div>
-            </h1>
-            <div className="hero__links">
-                <Link className="hero__link-stuff" to="/login">
-                    Oddaj rzeczy
-                </Link>
-                <Link className="hero__link-organise" to="/register">
-                    Zorganizuj zbiórkę
-                </Link>
+            <div className={"hero-container"}>
+                <header className="hero-container__header">
+                    <h1>Zacznij pomagać!</h1>
+                    <h1>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                </header>
+                <div className="hero-container__links">
+                    <Link className="hero-container__link-stuff" to="/login">
+                        Oddaj<br/> rzeczy
+                    </Link>
+                    <Link className="hero-container__link-organise" to="/login">
+                        Zorganizuj<br/> zbiórkę
+                    </Link>
+                </div>
             </div>
         </section>
-
     )
 }
--- donate-nearby/src/components/Navigation/Navigation.js
+++ donate-nearby/src/components/Navigation/Navigation.js
@@ -4,48 +4,48 @@ import React from "react";
 
 export function Navigation() {
     return (
-            <nav className="header">
-                <section className="header-login-section">
-                    <div className="header-login-section__login">
+            <nav className="navbar">
+                <div className="navbar-login-section">
+                    <div className="navbar-login-section__login">
                         <NavLink to="/login">
                             Zaloguj
                         </NavLink>
                     </div>
-                    <div className="header-login-section__register">
+                    <div className="navbar-login-section__register">
                         <NavLink to={"/register"}>
                             Załóż konto
                         </NavLink>
                     </div>
-                </section>
-                <section className="header-nav-section">
-                    <ul className="header-nav-section__ul">
-                        <li className="header-nav-section__ul__li">
+                </div>
+                <div className="navbar-nav-section">
+                    <ul className="navbar-nav-section__ul">
+                        <li className="navbar-nav-section__ul__li">
                             <Link>
                                 Start
                             </Link>
                         </li>
-                        <li className="header-nav-section__ul__li">
+                        <li className="navbar-nav-section__ul__li">
                             <Link to="statistics" spy={true} smooth={true} delay={500} duration={500}>
                                 O co chodzi?
                             </Link>
                         </li>
-                        <li className="header-nav-section__ul__li">
+                        <li className="navbar-nav-section__ul__li">
                             <Link>
                                 O nas
                             </Link>
                         </li>
-                        <li className="header-nav-section__ul__li">
+                        <li className="navbar-nav-section__ul__li">
                             <Link>
                                 Fundacje i organizacje
                             </Link>
                         </li>
-                        <li className="header-nav-section__ul__li">
+                        <li className="navbar-nav-section__ul__li">
                             <Link>
                                 Kontakt
                             </Link>
                         </li>
                     </ul>
-                </section>
+                </div>
             </nav>
     )
 }
