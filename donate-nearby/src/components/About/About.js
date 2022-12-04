0a4767693623dba6f5539f154ff0186f324c3410
Remek-P <remek.piotrowski@wp.pl>
2022-12-04 12:29:27 +0100

    Hero Component - static Statistics component done - static CTA Component - static About Component - started

--- donate-nearby/src/components/About/About.js
+++ donate-nearby/src/components/About/About.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export function About() {
     return(
-        <div>About</div>
+        <section className="about" id="about">
+            <div className="about-container">
+                <h2 className="about-container__header">O nas</h2>
+                <p className="about-container__description">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+            </div>
+        </section>
     )
 }
--- donate-nearby/src/components/CTA/CTA.js
+++ donate-nearby/src/components/CTA/CTA.js
@@ -10,19 +10,19 @@ export function CTA() {
             <ul className="cta-steps">
                 <li className="cta-steps-li cta-steps-li-icon1">
                     <h5 className="cta-steps-li__header">Wybierz rzeczy</h5>
-                    <p className="cta-steps-li__description">ubrania, zabawki, sprzęt i inne</p>
+                    <p className="cta-steps-li__description">ubrania, zabawki,<br/> sprzęt i inne</p>
                 </li>
                 <li className="cta-steps-li cta-steps-li-icon2">
                     <h5 className="cta-steps-li__header">Spakuj je</h5>
-                    <p className="cta-steps-li__description">skorzystaj z worków na śmieci</p>
+                    <p className="cta-steps-li__description">skorzystaj z<br/> worków na śmieci</p>
                 </li>
                 <li className="cta-steps-li cta-steps-li-icon3">
-                    <h5 className="cta-steps-li__header">Zdecyduj komu chcesz pomóc</h5>
-                    <p className="cta-steps-li__description">wybierz zaufane miejsce</p>
+                    <h5 className="cta-steps-li__header">Zdecyduj komu<br/> chcesz pomóc</h5>
+                    <p className="cta-steps-li__description">wybierz zaufane<br/> miejsce</p>
                 </li>
                 <li className="cta-steps-li cta-steps-li-icon4">
                     <h5 className="cta-steps-li__header">Zamów kuriera</h5>
-                    <p className="cta-steps-li__description">kurier przyjedzie w dogodnym terminie</p>
+                    <p className="cta-steps-li__description">kurier przyjedzie<br/> w dogodnym terminie</p>
                 </li>
             </ul>
             <Link to="/login" className="cta-link">
