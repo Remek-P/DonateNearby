72bdc234abdaa10b692423fb5400925ea86db261
Remek-P <remek.piotrowski@wp.pl>
2022-12-08 17:11:21 +0100

    Static Components: Hero, Statistics, CTA, About, Contact, Footer
    Login - partial validation, requires double-click

--- donate-nearby/src/components/Footer/Footer.js
+++ donate-nearby/src/components/Footer/Footer.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import facebookIcon from "../../assets/icons/Facebook.svg";
+import instagramIcon from "../../assets/icons/Instagram.svg";
+
 export function Footer() {
     return (
-        <div>Footer</div>
+        <section className="footer">
+            <div className="footer__r">Copyright by Coders Lab</div>
+            <div className="footer-icons">
+                <div
+                    className="footer-icons__icon1"
+                    style={{backgroundImage: `url(${facebookIcon})`}}
+                ></div>
+                <div
+                    className="footer-icons__icon2"
+                    style={{backgroundImage: `url(${instagramIcon})`}}
+                ></div>
+            </div>
+        </section>
     )
 }
