8a5f44eb99931bd062e67220d5f08698de120260
Remek-P <remek.piotrowski@wp.pl>
2022-12-08 17:51:25 +0100

    React-scroll adjustments
    Static Components:
    Hero, Statistics, CTA, About, Contact, Footer
    
    Login - partial validation, requires double-click

--- donate-nearby/src/components/Navigation/NavigationBar/NavigatinBar.js
+++ donate-nearby/src/components/Navigation/NavigationBar/NavigatinBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-scroll";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export function NavigationBar() {
 
