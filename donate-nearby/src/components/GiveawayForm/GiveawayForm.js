7c8054ba292aa16e374799b6ff1b34bc2f84d1d1
Remek-P <remek.piotrowski@wp.pl>
2022-12-09 20:00:26 +0100

    GiveawayForm - started
    Static Components:
    Hero, Statistics, CTA, About, Contact, Footer,GiveawayHero
    
    Login - partial validation, requires double-click

--- donate-nearby/src/components/GiveawayForm/GiveawayForm.js
+++ donate-nearby/src/components/GiveawayForm/GiveawayForm.js
@@ -3,6 +3,18 @@ import React from "react";
 export function GiveawayForm() {
 
     return (
-        <section>GiveawayForm</section>
+        <section className="giveaway-form">
+            <div className="giveaway-form__notification">
+                <h4 className="giveaway-form__notification__header">
+                    Ważne!
+                </h4>
+                <div className="giveaway-form__notification__paragraph">
+                    Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.
+                </div>
+            </div>
+            <div className="giveaway-form">
+
+            </div>
+        </section>
     )
 }
