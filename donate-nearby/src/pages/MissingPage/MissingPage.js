e12d53446499f5b69e7b2f60223f01406922d95d
Remek-P <remek.piotrowski@wp.pl>
2022-12-17 10:54:39 +0100

    MissingPage: with automatic redirect after 5 seconds
    Static Components:
    Hero, Statistics, CTA, About, Contact, Footer,GiveawayHero
    
    Login - partial validation, requires double-click

--- donate-nearby/src/pages/MissingPage/MissingPage.js
+++ donate-nearby/src/pages/MissingPage/MissingPage.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 
 import image from "../../assets/images/This-is-not-the-page-you-are-looking-for.png"
 
 export default function MissingPage() {
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const timerID = setTimeout(() => {
+            navigate(-1)
+        }, 5000)
+
+        return () => clearTimeout(timerID)
+    }, [])
+
     return (
         <>
             <h2>This is not the page you are looking for</h2>
