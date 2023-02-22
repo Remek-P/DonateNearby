410c1830514cbc2eecd525ca6889670234346cbb
Remek-P <remek.piotrowski@wp.pl>
2023-02-22 13:50:19 +0100

    CarouselFormStepThankYou - added redirection after 5s
    Static Components:
    Hero, Statistics, CTA, About, Contact, Footer,GiveawayHero
    
    Login - partial validation
    Forms - need to handle submission

--- donate-nearby/src/components/Giveaway/GiveawayCarouselForm/CarouselFormSteps/CarouselFormStepThankYou/CarouselFormStepThankYou.js
+++ donate-nearby/src/components/Giveaway/GiveawayCarouselForm/CarouselFormSteps/CarouselFormStepThankYou/CarouselFormStepThankYou.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function CarouselFormStepThankYou() {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeID = setTimeout(() => {
+            navigate("/")
+        }, 5000);
+
+        return () => clearTimeout(timeID)
+    })
 
     return (
         <h3 className="thankYou__header">Dziękujemy za przesłanie formularza<br/> Na maila prześlemy wszelkie informacje o odbiorze.
